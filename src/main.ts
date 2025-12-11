c76bb9671f0ce3bfffca99a918620f53fb3bd7ea
Deflnam <burukov01@bk.ru>
2025-12-11 19:35:22 +0300

    Deploy

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000','https://shop-barlos-client.onrender.com'],
   });
 
   const config = new DocumentBuilder()
